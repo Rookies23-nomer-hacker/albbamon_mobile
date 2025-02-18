27e3ecdf5f9d8d981b651e878e909ad9a4f44080
choi-seungho <151109075+choi-seungho@users.noreply.github.com>
2025-02-18 17:33:05 +0900

    test-commit
    try test

--- app/src/main/java/com/examplealbbamon/account/account.java
+++ app/src/main/java/com/examplealbbamon/account/account.java
@@ -1,14 +1,15 @@
-package com.examplealbbamon;
+package com.examplealbbamon.account;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.examplealbbamon.R;
+
+public class account extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
