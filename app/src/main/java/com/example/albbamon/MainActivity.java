aa9c12f0e0bd0082a178ea8c19af8b0d4ee2225f
YeonieKim <skc05049@gmail.com>
2025-02-20 10:55:50 +0900

    main

--- app/src/main/java/com/example/albbamon/MainActivity.java
+++ app/src/main/java/com/example/albbamon/MainActivity.java
@@ -1,24 +1,34 @@
 package com.example.albbamon;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import androidx.appcompat.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // 버튼 초기화
+        Button btnOpenMypage = findViewById(R.id.btn_open_mypage);
+
+        // 버튼 클릭 시 UserMypage 이동
+        btnOpenMypage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, UserMypage.class);
+                startActivity(intent);
+            }
         });
     }
 }
